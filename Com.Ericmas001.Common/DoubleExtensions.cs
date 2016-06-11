57d6b403cf5fc313d9ed78031f1fe3ded11c82ef
Éric Massé <Ericmas001@gmail.com>
2016-06-11 14:29:20 -0400

    double.Is

---
+++ Com.Ericmas001.Common/DoubleExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ericmas001.Common
+{
+    public static class DoubleExtensions
+    {
+        public static bool Is(this double d1, double d2)
+        {
+            return Math.Abs(d1 - d2) < double.Epsilon;
+        }
+    }
+}
